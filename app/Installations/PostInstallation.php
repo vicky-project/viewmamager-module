d3eba83c641166add693b59fe331b5010df161a8
vicky <pratamavr@gmail.com>
2025-11-29 22:16:11 +0800

    menambahkan post installation

---
+++ app/Installations/PostInstallation.php
@@ -0,0 +1,25 @@
+<?php
+namespace Modules\ViewManager\Installations;
+
+use Nwidart\Modules\Facades\Module;
+use Illuminate\Support\Facades\Artisan;
+
+class PostInstallation
+{
+	public function handle(string $moduleName)
+	{
+		try {
+			$module = Module::find($moduleName);
+			$module->enable();
+
+			Artisan::call("view:install", ["--force" => true]);
+		} catch (\Exception $e) {
+			logger()->error(
+				"Failed to running post installation of view mamager: " .
+					$e->getMessage()
+			);
+
+			throw $e;
+		}
+	}
+}
